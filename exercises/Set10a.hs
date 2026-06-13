12645e38ba0269ce7ad713a153ed3a099d7a5511
= <=>
2026-06-13 11:49:36 +0200

    exercice 10a 1-9

--- exercises/Set10a.hs
+++ exercises/Set10a.hs
@@ -16,7 +16,7 @@ import Mooc.Todo
 --   take 10 (doublify [0..])  ==>  [0,0,1,1,2,2,3,3,4,4]
 
 doublify :: [a] -> [a]
-doublify = todo
+doublify = foldr (\ x acc -> x:x:acc) []  
 
 ------------------------------------------------------------------------------
 -- Ex 2: Implement the function interleave that takes two lists and
@@ -37,7 +37,9 @@ doublify = todo
 --   take 10 (interleave [1..] (repeat 0)) ==> [1,0,2,0,3,0,4,0,5,0]
 
 interleave :: [a] -> [a] -> [a]
-interleave = todo
+interleave xs [] = xs
+interleave [] xs = xs
+interleave (x:xs) (y:ys)= x:y: (interleave xs ys)
 
 ------------------------------------------------------------------------------
 -- Ex 3: Deal out cards. Given a list of players (strings), and a list
@@ -56,7 +58,8 @@ interleave = todo
 -- Hint: remember the functions cycle and zip?
 
 deal :: [String] -> [String] -> [(String,String)]
-deal = todo
+deal [] _ = []
+deal ps cards = zip  cards (cycle ps)
 
 ------------------------------------------------------------------------------
 -- Ex 4: Compute a running average. Go through a list of Doubles and
@@ -74,7 +77,9 @@ deal = todo
 
 
 averages :: [Double] -> [Double]
-averages = todo
+averages [] = [] 
+averages (x:xs) = map (\ (i,v) -> v/i ) (zip [1..]  (scanl (+) x xs  ))
+
 
 ------------------------------------------------------------------------------
 -- Ex 5: Given two lists, xs and ys, and an element z, generate an
@@ -92,7 +97,7 @@ averages = todo
 --   take 10 (alternate [1,2] [3,4,5] 0) ==> [1,2,0,3,4,5,0,1,2,0]
 
 alternate :: [a] -> [a] -> a -> [a]
-alternate xs ys z = todo
+alternate xs ys z = cycle (xs ++ [z] ++ ys ++ [z])
 
 ------------------------------------------------------------------------------
 -- Ex 6: Check if the length of a list is at least n. Make sure your
@@ -104,7 +109,7 @@ alternate xs ys z = todo
 --   lengthAtLeast 10 [0..]  ==> True
 
 lengthAtLeast :: Int -> [a] -> Bool
-lengthAtLeast = todo
+lengthAtLeast n xs = length (take n xs) == n 
 
 ------------------------------------------------------------------------------
 -- Ex 7: The function chunks should take in a list, and a number n,
@@ -122,7 +127,18 @@ lengthAtLeast = todo
 --   take 4 (chunks 3 [0..]) ==> [[0,1,2],[1,2,3],[2,3,4],[3,4,5]]
 
 chunks :: Int -> [a] -> [[a]]
-chunks = todo
+chunks _ [] = [] 
+chunks n xs = let 
+                v =  (take n xs) 
+                
+              in 
+                if length v == n 
+                    then 
+                        v: chunks n (drop 1 xs)
+                    else 
+                        [] 
+
+
 
 ------------------------------------------------------------------------------
 -- Ex 8: Define a newtype called IgnoreCase, that wraps a value of
@@ -137,8 +153,15 @@ chunks = todo
 -- Examples:
 --   ignorecase "abC" == ignorecase "ABc"  ==>  True
 --   ignorecase "acC" == ignorecase "ABc"  ==>  False
+ignorecase :: String ->  IgnoreCase 
+ignorecase xs  = IgnoreCase (foldr (\ s acc   ->   (Data.Char.toLower  s) : acc) "" xs)
 
-ignorecase = todo
+newtype IgnoreCase = IgnoreCase String  
+
+instance  Eq IgnoreCase  where
+      (==) (IgnoreCase s1) (IgnoreCase s2) =   s1 == s2 
+
+
 
 ------------------------------------------------------------------------------
 -- Ex 9: Here's the Room type and some helper functions from the
@@ -182,4 +205,22 @@ play room (d:ds) = case move room d of Nothing -> [describe room]
                                        Just r -> describe room : play r ds
 
 maze :: Room
-maze = todo
+maze = maze1
+--  maze1 has the description "Maze"
+--    * The direction "Left" goes to maze2
+--    * "Right" goes to maze3
+maze1 :: Room
+maze1 = Room "Maze" [("Left" , maze2) , ("Right", maze3)]
+
+--  * maze2 has the description "Deeper in the maze"
+--    * "Left" goes to maze3
+--    * "Right" goes to maze1
+maze2 :: Room
+maze2 = Room "Deeper in the maze" [("Left" , maze3) , ("Right" , maze1)]
+
+--  * maze3 has the description "Elsewhere in the maze"
+--    * "Left" goes to maze1
+--    * "Right" goes to maze2
+
+maze3 :: Room
+maze3 = Room "Elsewhere in the maze" [("Left" , maze1) , ("Right" , maze2)]
